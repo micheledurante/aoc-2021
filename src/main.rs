2e7d92bd1b1889866f33aafd41602147af41a518
micheled <me@thisisdurante.com>
2022-08-15 17:12:13 +0200

    Add code for day 2

--- src/main.rs
+++ src/main.rs
@@ -5,6 +5,7 @@ use std::fs::File;
 use std::io::{BufRead, BufReader};
 
 mod day_01;
+mod day_02;
 
 const AOC_2021_SRC_PATH: &str = "AOC_2021_SRC_PATH";
 
@@ -12,13 +13,13 @@ fn print_solution(day: i32, part: i32, content: &str) -> () {
     println!("Day {}, part {}: {}", day, part, content)
 }
 
-fn read_puzzle_input(filename: &str) -> Vec<i64> {
+fn read_puzzle_input(filename: &str) -> Vec<String> {
     let file = File::open(env::var(AOC_2021_SRC_PATH).unwrap() + filename).unwrap();
     let lines = BufReader::new(file).lines();
     let mut input = vec![];
 
     for line in lines {
-        input.push(line.unwrap().parse::<i64>().unwrap())
+        input.push(line.unwrap())
     }
 
     input
@@ -27,4 +28,6 @@ fn read_puzzle_input(filename: &str) -> Vec<i64> {
 fn main() {
     day_01::part_1();
     day_01::part_2();
+    day_02::part_1();
+    day_02::part_2();
 }
