0174431216c83b975d1ee4df12476a0220981125
micheled <me@thisisdurante.com>
2022-09-21 11:57:44 +0200

    Complete day 8

--- AoC2021/AoC2021/Day8.cs
+++ AoC2021/AoC2021/Day8.cs
@@ -9,30 +9,105 @@ namespace AoC2021
 {
     public class Day8
     {
+        public static Dictionary<char, char> FindSegments(List<HashSet<char>> patterns)
+        {
+            var segments = new Dictionary<char, char>();
+
+            var bd = new HashSet<char>(); // 4 - 1
+            bd.UnionWith(patterns[2].Except(patterns[0]));
+
+            var cde = new HashSet<char>(); // [c,d,e] = (8 - 0) + (8 - 6) + (8 - 9)
+            cde.UnionWith(patterns[9].Except(patterns[6]));
+            cde.UnionWith(patterns[9].Except(patterns[7]));
+            cde.UnionWith(patterns[9].Except(patterns[8]));
+
+            var bcef = new HashSet<char>(); // [b,c,e,f] = (8 - 2) + (8 - 3) + (8 - 5)
+            bcef.UnionWith(patterns[9].Except(patterns[3]));
+            bcef.UnionWith(patterns[9].Except(patterns[4]));
+            bcef.UnionWith(patterns[9].Except(patterns[5]));
+
+            var de = new HashSet<char>(); // 4 - [b,c,e,f]
+            // equivalent to mathematical ∆
+            // https://stackoverflow.com/a/5620306
+            de.UnionWith(patterns[2]);
+            de.SymmetricExceptWith(bcef);
+
+            segments.Add('a', patterns[1].Except(patterns[0]).First()); // a = 7 - 1
+            segments.Add('c', cde.Except(de).First()); // c = [c,d,e] - [d,e]
+            segments.Add('d', bd.Intersect(de).First()); // d = [b,d] ⋂ [d,e]
+            segments.Add('e', cde.Except(new HashSet<char> { segments['c'] }).Except(new HashSet<char> { segments['d'] }).First()); // e = [c,d,e] - c - d
+            segments.Add('f', patterns[0].Except(new HashSet<char> { segments['c'] }).First()); // f = 1 - c
+            segments.Add('b', bd.Except(new HashSet<char> { segments['d'] }).First()); // b = [b,d] - d
+            segments.Add('g', patterns[3].Except(patterns[2]).Except(new HashSet<char> { segments['a'] }).Except(new HashSet<char> { segments['e'] }).First()); // g = 8 - 4 - a - e
+
+            return segments;
+        }
+
+        public static string GetDigit(Dictionary<char, char> segments, HashSet<char> number)
+        {
+            if (number.Except(new HashSet<char> { segments['a'], segments['c'], segments['d'], segments['e'], segments['g'] }).Count() == 0)
+                return "2";
+            else if (number.Except(new HashSet<char> { segments['a'], segments['c'], segments['d'], segments['f'], segments['g'] }).Count() == 0)
+                return "3";
+            else if (number.Except(new HashSet<char> { segments['a'], segments['b'], segments['d'], segments['f'], segments['g'] }).Count() == 0)
+                return "5";
+            else if (number.Except(new HashSet<char> { segments['a'], segments['b'], segments['c'], segments['e'], segments['f'], segments['g'] }).Count() == 0)
+                return "0";
+            else if (number.Except(new HashSet<char> { segments['a'], segments['b'], segments['d'], segments['e'], segments['f'], segments['g'] }).Count() == 0)
+                return "6";
+            else if (number.Except(new HashSet<char> { segments['a'], segments['b'], segments['c'], segments['d'], segments['f'], segments['g'] }).Count() == 0)
+                return "9";
+
+            return null;
+        }
+
+        public static int DecodeReading(Dictionary<char, char> segments, List<HashSet<char>> reading)
+        {
+            var number = new string[4];
+
+            for (var i = 0; i < 4; i++)
+            {
+                if (reading[i].Count() == 2)
+                    number[i] = "1";
+                else if (reading[i].Count() == 3)
+                    number[i] = "7";
+                else if (reading[i].Count() == 4)
+                    number[i] = "4";
+                else if (reading[i].Count() == 5)
+                    number[i] = GetDigit(segments, reading[i]);
+                else if (reading[i].Count() == 6)
+                    number[i] = GetDigit(segments, reading[i]);
+                else if (reading[i].Count() == 7)
+                    number[i] = "8";
+            }
+
+            return int.Parse(string.Concat(number));
+        }
+
         public static (int, int) Run(string[] input)
         {
             var readings = input
                 .Select(x => x.Split(new string[] { "|" }, StringSplitOptions.RemoveEmptyEntries))
                 .Select(
                     y => (
-                        y[0].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(z => new HashSet<char>(z)).OrderBy(q => q.Count()).ToList(),
-                        y[1].Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries).Select(z => new HashSet<char>(z)).OrderBy(q => q.Count()).ToList()
+                        y[0]
+                            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(z => new HashSet<char>(z))
+                            .OrderBy(q => q.Count())
+                            .ToList(),
+                        y[1]
+                            .Split(new char[] { ' ' }, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(z => new HashSet<char>(z))
+                            .ToList()
                     )
                 )
                 .ToList();
-            var nums = new List<int>();
-
-            foreach (var reading in readings)
-            {
-                
-            }
-
 
             return (
                 readings
                     .Select(v => v.Item2.Where(x => x.Count() == 2 || x.Count() == 3 || x.Count() == 4 || x.Count() == 7).ToList().Count())
                     .Sum(),
-                0
+                readings.Select(x => DecodeReading(FindSegments(x.Item1), x.Item2)).Sum()
             );
         }
     }
---
+++ AoC2021Tests/AoC2021Tests/Day08Tests.cs
@@ -0,0 +1,173 @@
+﻿using AoC2021;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AoC2021Tests
+{
+    [TestClass]
+    public class Day08Tests : UnitTest
+    {
+        private Dictionary<char, char> segments;
+
+        [TestInitialize]
+        /// Validity of the dictionary is tested in FindSegments_GivenAnOrderedPattern_ShouldFindSegments()
+        public void Initialize()
+        {
+            segments = new Dictionary<char, char>();
+            segments.Add('a', 'd');
+            segments.Add('b', 'e');
+            segments.Add('c', 'a');
+            segments.Add('d', 'f');
+            segments.Add('e', 'g');
+            segments.Add('f', 'b');
+            segments.Add('g', 'c');
+        }
+
+        [TestMethod]
+        public void FindSegments_GivenAnOrderedPattern_ShouldFindSegments()
+        {
+            var input = new List<HashSet<char>>();
+            input.Add(new HashSet<char> { 'a', 'b' }); //acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab
+            input.Add(new HashSet<char> { 'd', 'a', 'b' }); //acedgfb cdfbe gcdfa fbcad cefabd cdfgeb eafb cagedb
+            input.Add(new HashSet<char> { 'e', 'a', 'f', 'b' }); //acedgfb cdfbe gcdfa fbcad cefabd cdfgeb cagedb
+            input.Add(new HashSet<char> { 'c', 'd', 'f', 'b', 'e' }); //acedgfb gcdfa fbcad cefabd cdfgeb cagedb
+            input.Add(new HashSet<char> { 'g', 'c', 'd', 'f', 'a' }); //acedgfb fbcad cefabd cdfgeb cagedb
+            input.Add(new HashSet<char> { 'f', 'b', 'c', 'a', 'd' }); //acedgfb cefabd cdfgeb cagedb
+            input.Add(new HashSet<char> { 'c', 'e', 'f', 'a', 'b', 'd' }); //acedgfb cdfgeb cagedb
+            input.Add(new HashSet<char> { 'c', 'd', 'f', 'g', 'e', 'b' }); //acedgfb cagedb
+            input.Add(new HashSet<char> { 'c', 'a', 'g', 'e', 'd', 'b' }); //acedgfb
+            input.Add(new HashSet<char> { 'a', 'c', 'e', 'd', 'g', 'f', 'b' });
+
+            var result = Day8.FindSegments(input);
+
+            Assert.AreEqual(segments['a'], result['a']);
+            Assert.AreEqual(segments['b'], result['b']);
+            Assert.AreEqual(segments['c'], result['c']);
+            Assert.AreEqual(segments['d'], result['d']);
+            Assert.AreEqual(segments['e'], result['e']);
+            Assert.AreEqual(segments['f'], result['f']);
+            Assert.AreEqual(segments['g'], result['g']);
+        }
+
+        [TestMethod]
+        public void GetDigit_5Letters_ShouldReturn2()
+        {
+            var input = new HashSet<char> { 'g', 'c', 'd', 'f', 'a' };
+
+            var result = Day8.GetDigit(segments, input);
+
+            Assert.AreEqual("2", result);
+        }
+
+        [TestMethod]
+        public void GetDigit_5Letters_ShouldReturn3()
+        {
+            var input = new HashSet<char> { 'f', 'b', 'c', 'a', 'd' };
+
+            var result = Day8.GetDigit(segments, input);
+
+            Assert.AreEqual("3", result);
+        }
+
+        [TestMethod]
+        public void GetDigit_5Letters_ShouldReturn5()
+        {
+            var input = new HashSet<char> { 'c', 'd', 'f', 'b', 'e' };
+
+            var result = Day8.GetDigit(segments, input);
+
+            Assert.AreEqual("5", result);
+        }
+
+        [TestMethod]
+        public void GetDigit_6Letters_ShouldReturn0()
+        {
+            var input = new HashSet<char> { 'c', 'a', 'g', 'e', 'd', 'b' };
+
+            var result = Day8.GetDigit(segments, input);
+
+            Assert.AreEqual("0", result);
+        }
+
+        [TestMethod]
+        public void GetDigit_6Letters_ShouldReturn6()
+        {
+            var input = new HashSet<char> { 'c', 'd', 'f', 'g', 'b', 'e' };
+
+            var result = Day8.GetDigit(segments, input);
+
+            Assert.AreEqual("6", result);
+        }
+
+        [TestMethod]
+        public void GetDigit_6Letters_ShouldReturn9()
+        {
+            var input = new HashSet<char> { 'c', 'e', 'f', 'a', 'b', 'd' };
+
+            var result = Day8.GetDigit(segments, input);
+
+            Assert.AreEqual("9", result);
+        }
+
+        [TestMethod]
+        public void DecodeReading_GivenARreading1_ShoulReturnTheConcatNumber()
+        {
+            var num_1 = new HashSet<char> { 'c', 'd', 'f', 'e', 'b' };
+            var num_2 = new HashSet<char> { 'f', 'c', 'a', 'd', 'b' };
+            var num_3 = new HashSet<char> { 'c', 'd', 'f', 'e', 'b' };
+            var num_4 = new HashSet<char> { 'c', 'd', 'b', 'a', 'f' };
+            var input = new List<HashSet<char>> { num_1, num_2, num_3, num_4 };
+
+            var result = Day8.DecodeReading(segments, input);
+
+            Assert.AreEqual(5353, result);
+        }
+
+        [TestMethod]
+        public void DecodeReading_GivenARreading2_ShoulReturnTheConcatNumber()
+        {
+            var num_1 = new HashSet<char> { 'a', 'c', 'e', 'd', 'g', 'f', 'b' };
+            var num_2 = new HashSet<char> { 'd', 'a', 'b' };
+            var num_3 = new HashSet<char> { 'e', 'a', 'f', 'b' };
+            var num_4 = new HashSet<char> { 'a', 'b' };
+            var input = new List<HashSet<char>> { num_1, num_2, num_3, num_4 };
+
+            var result = Day8.DecodeReading(segments, input);
+
+            Assert.AreEqual(8741, result);
+        }
+
+        [TestMethod]
+        public void Run_SingleInput_ShoulCorrectlyCalculateNumbers()
+        {
+            var input = new string[] { "acedgfb cdfbe gcdfa fbcad dab cefabd cdfgeb eafb cagedb ab | cdfeb fcadb cdfeb cdbaf" };
+
+            var result = Day8.Run(input);
+
+            Assert.AreEqual((0, 5353), result);
+        }
+
+        [TestMethod]
+        public void Run_MultipleInputs_ShoulCorrectlyCalculateNumbers()
+        {
+            var input = new string[] {
+                "be cfbegad cbdgef fgaecd cgeb fdcge agebfd fecdb fabcd edb | fdgacbe cefdb cefbgd gcbe",
+                "edbfga begcd cbg gc gcadebf fbgde acbgfd abcde gfcbed gfec | fcgedb cgb dgebacf gc",
+                "fgaebd cg bdaec gdafb agbcfd gdcbef bgcad gfac gcb cdgabef | cg cg fdcagb cbg",
+                "fbegcd cbd adcefb dageb afcb bc aefdc ecdab fgdeca fcdbega | efabcd cedba gadfec cb",
+                "aecbfdg fbg gf bafeg dbefa fcge gcbea fcaegb dgceab fcbdga | gecf egdcabf bgf bfgea",
+                "fgeab ca afcebg bdacfeg cfaedg gcfdb baec bfadeg bafgc acf | gebdcfa ecba ca fadegcb",
+                "dbcfg fgd bdegcaf fgec aegbdf ecdfab fbedc dacgb gdcebf gf | cefg dcbef fcge gbcadfe",
+                "bdfegc cbegaf gecbf dfcage bdacg ed bedf ced adcbefg gebcd | ed bcgafe cdgba cbgef",
+                "egadfb cdbfeg cegd fecab cgb gbdefca cg fgcdab egfdb bfceg | gbdfcae bgc cg cgb",
+                "gcafb gcf dcaebfg ecagb gf abcdeg gaef cafbge fdbac fegbdc | fgae cfgab fg bagce"
+            };
+
+            var result = Day8.Run(input);
+
+            Assert.AreEqual((26, 61229), result);
+        }
+    }
+}
